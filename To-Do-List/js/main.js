fda754f89fb41a5ba44ad9f7a5930d77facb7b66
Mohammed Wasim <46522018+mohwasim97@users.noreply.github.com>
2021-08-05 19:11:02 +0100

    add an array to keep track of each to do list

--- To-Do-List/js/main.js
+++ To-Do-List/js/main.js
@@ -1,9 +1,21 @@
+let array = []; //empty array
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
+let deleteToDobutton = document.getElementById('deleteToDo');
 
 addToDoButton.addEventListener('click', function(){
 var paragraph = document.createElement('p');
 paragraph.innerText = inputField.value;
-toDoContainer.appendChild(paragraph);
+array.push(paragraph);
+console.log(array);
+
+toDoContainer.appendChild(array[array.length-1]);
+
+})
+
+deleteToDobutton.addEventListener('click', function(){
+    
+    toDoContainer.removeChild(toDoContainer.lastChild);
+
 })
