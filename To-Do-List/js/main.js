39c093705ed0b7492161226f8c0208a1a50ae9aa
Mohammed Wasim <46522018+mohwasim97@users.noreply.github.com>
2021-08-07 18:53:16 +0100

    reverted back to original add task solution

--- To-Do-List/js/main.js
+++ To-Do-List/js/main.js
@@ -1,21 +1,9 @@
-let array = []; //empty array
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
-let deleteToDobutton = document.getElementById('deleteToDo');
 
 addToDoButton.addEventListener('click', function(){
 var paragraph = document.createElement('p');
 paragraph.innerText = inputField.value;
-array.push(paragraph);
-console.log(array);
-
-toDoContainer.appendChild(array[array.length-1]);
-
-})
-
-deleteToDobutton.addEventListener('click', function(){
-    
-    toDoContainer.removeChild(toDoContainer.lastChild);
-
+toDoContainer.appendChild(paragraph);
 })
